487fd791a57997fdb1c0d6c5711638e7b814dd95
sardaraditi <sardaraditi17@gmail.com>
2023-10-04 12:19:03 +0530

    Changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'; //impot section
 
-@Component({
+@Component({ //component directive/decorator
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent { //class to write code here 
   title = 'project';
+//let/var/const city= 'pune'
+//any Data type
+//property
+city? : string ; //declaration of property
+
+city2 :string ='pune' //declaration + defination of property
+
+city3 = 'mumbai'
+
+constructor(){}  // to inject dependencies
+
+//life of hooks
+
+//functions
 }
